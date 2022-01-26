b6458039add6ec467a4aa4d71e3edb5d97d86cab
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2022-01-26 20:53:12 +0530

    fix: refactor

--- containers/CertificateLoader.js
+++ containers/CertificateLoader.js
@@ -2,7 +2,7 @@ import { RefreshIcon } from "@heroicons/react/outline";
 
 const CertificateLoader = () => (
   <div className="bg-white certificate-dimentions p-2 border-2 rounded-md mx-auto">
-    <div className="flex flex-row items-center h-full justify-center space-x-5 animate-spin">
+    <div className="flex flex-row items-center h-full justify-center space-x-5">
       <RefreshIcon className="h-5 w-5 animate-spin" />
     </div>
   </div>
